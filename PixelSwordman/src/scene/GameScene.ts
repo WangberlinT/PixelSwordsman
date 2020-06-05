b2892e235bd286647390e51b705dae31fb7450b1
WangberlinT <11711613@mail.sustech.edu.cn>
2020-06-05 18:44:44 +0800

    游戏主场景

---
+++ PixelSwordman/src/scene/GameScene.ts
@@ -0,0 +1,22 @@
+class GameScene extends Scene implements  eui.UIComponent {
+	public constructor() {
+		super();
+	}
+
+	protected partAdded(partName:string,instance:any):void
+	{
+		super.partAdded(partName,instance);
+	}
+
+
+	protected childrenCreated():void
+	{
+		super.childrenCreated();
+	}
+
+	
+	protected onComplete()
+	{
+		
+	}
+}
